882f79218081fcc016e72759450db4aeb95cc973
Kyle Yang <REmpire.dev@proton.me>
2025-07-31 19:23:48 -0600

    Update: Added back support for client side router

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
-const anchors = [...document.getElementsByTagName("a")];
-anchors.forEach(anchor => {
-  anchor.addEventListener("dragstart", function (e) {
-    e.preventDefault();  // Prevent default drag behavior
+import { App } from './app.js';
+
+function disableLinkDrag(body) {
+  // Disable dragging on all links
+  body.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener("dragstart", function (e) {
+      e.preventDefault();  // Prevent default drag behavior
+    });
   });
-});
+}
 
-if (!('ontouchstart' in window)) {  // Skip mouse effects on touch devices
-
-  /* Spotlight Effect */
-  // match the spotlight effect to elements with class 'spotlight-container-xx where xx is the radius
-  document.querySelectorAll('[class*="spotlight-container"]').forEach(el => {
-    // Extract radius from class name, default to 600 if not found
+// Initialize spotlight effects on elements with class 'spotlight-container'
+// radius can be set via class name i.e. 'spotlight-container-600'
+function initSpotlights(body) {
+  body.querySelectorAll('[class*="spotlight-container"]').forEach(el => {
     const match = el.className.match(/spotlight-container-(\d+)/);
-    const radius = match ? parseInt(match[1], 10) : 600;
+    const radius = match ? parseInt(match[1], 10) : 600;  // Default radius if not set
 
     el.addEventListener('mousemove', e => {
       // Get mouse position relative to viewport
       const x = e.clientX;
       const y = e.clientY;
       el.querySelectorAll('.spotlight').forEach(g => {
-        // Get spotlight element's bounding rect
+        // Place the spotlight at mouse position for each spotlight element
         const rect = g.getBoundingClientRect();
-        // Calculate mouse position relative to this spotlight element
         const localX = x - rect.left;
         const localY = y - rect.top;
-        g.style.backgroundImage = `radial-gradient(${radius}px at ${localX}px ${localY}px, rgba(255,255,255,0.2), transparent 70%)`;
+        g.style.backgroundImage = `radial-gradient(${radius}px at ${localX}px ${localY}px, #fff3, transparent 70%)`;
       });
     });
   });
+}
 
-  /* Link Stalker */
-  const stalker = document.getElementById('STALKER');
-  const stalkerLink = stalker.querySelector('.stalker-link');
-  // Move stalker under mouse
-  document.addEventListener('mousemove', (e) => {
-    stalker.style.transform = `translate(${e.clientX}px, ${e.clientY + 24}px)`;
-  });
-  // Update stalker link on hover
-  anchors.forEach(link => {
+// Display link href on hover
+const stalker = document.getElementById('STALKER');
+const stalkerInner = stalker.querySelector('#STALKER_INNER');
+const stalkerLink = stalker.querySelector('.stalker-link');
+
+// Stalker follows mouse
+document.addEventListener('mousemove', (e) => {
+  stalker.style.transform = `translate(${e.clientX}px, ${e.clientY + 24}px)`;
+});
+
+function updateStalkerLinks(body) {
+  body.querySelectorAll('a').forEach(link => {
     // Skip with class no-stalker
     if (link.classList.contains('no-stalker')) return;
+
     link.addEventListener('mouseenter', (e) => {
-      const stalkerInner = document.getElementById('STALKER_INNER');
       stalkerInner.style.opacity = '1';
       stalkerInner.style.filter = 'blur(0px)';
-      stalkerInner.style.transitionDuration = '0.4s';
-      stalkerLink.textContent = link.href;
+      stalkerInner.style.transitionDuration = '.4s';
+      stalkerLink.textContent = link.href.replace(/^https?:\/\//, '');
       const width = stalkerInner.offsetWidth;
       stalkerInner.style.marginLeft = `-${width / 2}px`;
+      stalker.style.transform = `translate(${e.clientX}px, ${e.clientY + 24}px)`;
     });
     link.addEventListener('mouseleave', (e) => {
-      const stalkerInner = document.getElementById('STALKER_INNER');
       stalkerInner.style.opacity = '0';
       stalkerInner.style.filter = 'blur(16px)';
       stalkerInner.style.transitionDuration = '1.2s';
     });
   });
-} else {
-  console.log('Touch device detected, skipping mouse effects');
 }
 
-/* Nothing to see here */
-var msg = `
-\r\n\r\n      ___           ___           ___           ___                     ___           ___     \r\n     \/  \/\\         \/  \/\\         \/__\/\\         \/  \/\\      ___          \/  \/\\         \/  \/\\    \r\n    \/  \/::\\       \/  \/:\/_       |  |::\\       \/  \/::\\    \/  \/\\        \/  \/::\\       \/  \/:\/_   \r\n   \/  \/:\/\\:\\     \/  \/:\/ \/\\      |  |:|:\\     \/  \/:\/\\:\\  \/  \/:\/       \/  \/:\/\\:\\     \/  \/:\/ \/\\  \r\n  \/  \/:\/~\/:\/    \/  \/:\/ \/:\/_   __|__|:|\\:\\   \/  \/:\/~\/:\/ \/__\/::\\      \/  \/:\/~\/:\/    \/  \/:\/ \/:\/_ \r\n \/__\/:\/ \/:\/___ \/__\/:\/ \/:\/ \/\\ \/__\/::::| \\:\\ \/__\/:\/ \/:\/  \\__\\\/\\:\\__  \/__\/:\/ \/:\/___ \/__\/:\/ \/:\/ \/\\\r\n \\  \\:\\\/:::::\/ \\  \\:\\\/:\/ \/:\/ \\  \\:\\~~\\__\\\/ \\  \\:\\\/:\/      \\  \\:\\\/\\ \\  \\:\\\/:::::\/ \\  \\:\\\/:\/ \/:\/\r\n  \\  \\::\/~~~~   \\  \\::\/ \/:\/   \\  \\:\\        \\  \\::\/        \\__\\::\/  \\  \\::\/~~~~   \\  \\::\/ \/:\/ \r\n   \\  \\:\\        \\  \\:\\\/:\/     \\  \\:\\        \\  \\:\\        \/__\/:\/    \\  \\:\\        \\  \\:\\\/:\/  \r\n    \\  \\:\\        \\  \\::\/       \\  \\:\\        \\  \\:\\       \\__\\\/      \\  \\:\\        \\  \\::\/   \r\n     \\__\\\/         \\__\\\/         \\__\\\/         \\__\\\/                   \\__\\\/         \\__\\\/    \r\n\r\n 
+updateStalkerLinks(document);
 
-  REVERSE ENGINEERING EMPIRE | RE-MPIRE`;
-console.log(msg);
+if ('ontouchstart' in window) {  // Skip mouse effects on touch devices
+  console.log('Touch device detected, skipping mouse effects');
+  initSpotlights = function () { }
+  initLinkDisplay = function () { }
+}
+
+// window.initPageContent = function (el) {
+export function initPageContent(el) {
+  disableLinkDrag(el);
+  initSpotlights(el);
+  updateStalkerLinks(el);
+}
